345a7c461c4fcbf02fc62286933a0348f1ade160
Antonio Marin Jimenez <antonio.marin.jimenez@gmail.com>
2020-07-18 04:50:33 +0200

    Loading all modules on tests

--- test/test.js
+++ test/test.js
@@ -3,20 +3,27 @@ var { assert } = require('chai');
 var searchquire = require('searchquire');
 
 describe('lodash library', () => {
-    let _array;
+    let _ = {};
 
     before(() => {
+        const config = {
+            basePath: '../cartridges/lib_lodash/cartridge',
+            pattern: '*/cartridge/(.*)',
+            maxSearchModuleIterations: 20
+        };
         // initialize test config and spies
-        _array = searchquire(
-            '*/cartridge/scripts/lib/lodash/array',
-            {
-                basePath: '../cartridges/lib_lodash/cartridge',
-                pattern: '*/cartridge/(.*)',
-                maxSearchModuleIterations: 20
-            },
-            {
-            }
-        );
+        _.array = searchquire('*/cartridge/scripts/lib/lodash/array', config);
+        _.chain = searchquire('*/cartridge/scripts/lib/lodash/chain', config);
+        _.collection = searchquire('*/cartridge/scripts/lib/lodash/collection', config);
+        _.date = searchquire('*/cartridge/scripts/lib/lodash/date', config);
+        _.function = searchquire('*/cartridge/scripts/lib/lodash/function', config);
+        _.lang = searchquire('*/cartridge/scripts/lib/lodash/lang', config);
+        _.math = searchquire('*/cartridge/scripts/lib/lodash/math', config);
+        _.number = searchquire('*/cartridge/scripts/lib/lodash/number', config);
+        _.object = searchquire('*/cartridge/scripts/lib/lodash/object', config);
+        _.string = searchquire('*/cartridge/scripts/lib/lodash/string', config);
+        _.support = searchquire('*/cartridge/scripts/lib/lodash/support', config);
+        _.utility = searchquire('*/cartridge/scripts/lib/lodash/utility', config);
     });
 
     beforeEach(() => {
@@ -25,7 +32,18 @@ describe('lodash library', () => {
 
     describe('lodash initialization', () => {
         it('lodash defined', () => {
-            assert.isDefined(_array);
+            assert.isDefined(_.array);
+            assert.isDefined(_.chain);
+            assert.isDefined(_.collection);
+            assert.isDefined(_.date);
+            assert.isDefined(_.function);
+            assert.isDefined(_.lang);
+            assert.isDefined(_.math);
+            assert.isDefined(_.number);
+            assert.isDefined(_.object);
+            assert.isDefined(_.string);
+            assert.isDefined(_.support);
+            assert.isDefined(_.utility);
         });
     });
 });
